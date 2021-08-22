25c25291d8ce9c8ddd66b6f9bd61f76feace0218
Ayman <ayman.patel97@gmail.com>
2021-08-22 12:04:23 +0530

    eat: Vue Mastery (Lesson 5) API with Axios

---
+++ 2_vue/real-world-vue/src/services/EventService.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getEvents() {
+    return apiClient.get("/events");
+  },
+};
--- 2_vue/real-world-vue/src/views/EventList.vue
+++ 2_vue/real-world-vue/src/views/EventList.vue
@@ -10,40 +10,65 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService";
 
 export default {
   name: "Home",
   components: {
     EventCard,
   },
+
+  created() {
+    // axios
+    //   .get(
+    //     "https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events"
+    //   )
+
+    EventService.getEvents()
+      .then((response) => {
+        console.log(`events: ${response.data}`);
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
   data() {
     return {
-      events: [
-        {
-          id: 439842032,
-          category: "front-end",
-          title: "Vue is great!",
-          description: "Vue is for dev by dev",
-          location: "China",
-          data: "August 22, 2021",
-          time: "12:00",
-          petsAllowed: false,
-          organizer: "Ayman",
-        },
-        {
-          id: 439842032,
-          category: "front-end",
-          title: "Vue is great!",
-          description: "Vue is for dev by dev",
-          location: "China",
-          data: "August 22, 2021",
-          time: "12:00",
-          petsAllowed: false,
-          organizer: "Ayman",
-        },
-      ],
+      events: null,
     };
   },
+
+  // Static Data
+  // data() {
+  //   return {
+  //     events: [
+  //       {
+  //         id: 439842032,
+  //         category: "front-end",
+  //         title: "Vue is great!",
+  //         description: "Vue is for dev by dev",
+  //         location: "China",
+  //         data: "August 22, 2021",
+  //         time: "12:00",
+  //         petsAllowed: false,
+  //         organizer: "Ayman",
+  //       },
+  //       {
+  //         id: 439842032,
+  //         category: "front-end",
+  //         title: "Vue is great!",
+  //         description: "Vue is for dev by dev",
+  //         location: "China",
+  //         data: "August 22, 2021",
+  //         time: "12:00",
+  //         petsAllowed: false,
+  //         organizer: "Ayman",
+  //       },
+  //     ],
+  //   };
+  // },
 };
 </script>
 
